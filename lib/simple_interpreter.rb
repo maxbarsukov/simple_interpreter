5c74f66522caadc88ec09aa1a57b487df646bf28
maxbarsukov <maximbarsukov@bk.ru>
2021-05-26 20:09:41 +0300

    Lexer updated

--- lib/simple_interpreter.rb
+++ lib/simple_interpreter.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
 require_relative "simple_interpreter/version"
+require_relative 'simple_interpreter/lexer'
+
 
 module SimpleInterpreter
 end
+
+
+code = 'sum ASSIGN 5 MINUS 9;'
+
+lexer = Lexer.new(code)
+
+lexer.analyze
+
+puts lexer.token_list.map(&:type).map(&:name)
--- lib/simple_interpreter/lexer.rb
+++ lib/simple_interpreter/lexer.rb
@@ -1,4 +1,5 @@
 require_relative 'token'
+require_relative 'token_types_list'
 
 class Lexer
   attr_accessor :code,
@@ -12,13 +13,29 @@ class Lexer
   end
 
   def analyze
-    while next_token
-      # some analyze here
+    while next_token; end
+
+    @token_list.filter do |token|
+      token.type.name != TokenTypesList::TOKEN_TYPES['SPACE'].name
     end
-    token_list
   end
 
   def next_token
+    return false if @position >= @code.size
 
+    token_types = TokenTypesList::TOKEN_TYPES.values
+
+    token_types.each do |token_type|
+      regex = Regexp.new('^' << token_type.regex)
+      result = @code[position..].scan(regex)
+
+      unless result.nil? || result.empty? || result[0].nil? || result[0].empty?
+        token = Token.new(token_type, result[0], @position)
+        @position += result[0].size
+        @token_list << token
+        return true
+      end
+    end
+    raise "Script error on line #{@position}"
   end
 end
