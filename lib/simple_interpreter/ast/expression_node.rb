5adc0935f25f85544c00127f0e962f5168e8026c
maxbarsukov <maximbarsukov@bk.ru>
2021-05-26 20:44:43 +0300

    Statements node created

---
+++ lib/simple_interpreter/ast/expression_node.rb
@@ -0,0 +1,1 @@
+class ExpressionNode; end
--- lib/simple_interpreter/ast/statements_node.rb
+++ lib/simple_interpreter/ast/statements_node.rb
@@ -1,1 +1,9 @@
+require_relative 'expression_node'
 
+class StatementsNode < ExpressionNode
+  attr_accessor :code_strings
+
+  def add_node(node)
+    @code_strings << node
+  end
+end
