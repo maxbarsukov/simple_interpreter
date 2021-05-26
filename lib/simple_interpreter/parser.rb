d015fde782e55bd7cc684f98d8a23447c4d20443
maxbarsukov <maximbarsukov@bk.ru>
2021-05-26 21:24:21 +0300

    Some parsers created

--- lib/simple_interpreter/parser.rb
+++ lib/simple_interpreter/parser.rb
@@ -2,6 +2,13 @@ require_relative 'token'
 require_relative 'token_type'
 require_relative 'token_types_list'
 
+require_relative 'ast/statements_node'
+require_relative 'ast/expression_node'
+require_relative 'ast/number_node'
+require_relative 'ast/variable_node'
+require_relative 'ast/bin_operation_node'
+require_relative 'ast/unar_operation_node'
+
 class Parser
   attr_accessor :tokens,
                 :position,
@@ -13,7 +20,46 @@ class Parser
     @scope = {}
   end
 
-  def require(*expected)
+  def parse_code
+    root = StatementsNode.new
+    while @position < @tokens.size
+      code_string_node = parse_expression
+      require_token TokenTypesList::TOKEN_TYPES['SEMICOLON']
+      root.add_node code_string_node
+    end
+
+    root
+  end
+
+  def parse_expression
+    unless match(TokenTypesList::TOKEN_TYPES['VARIABLE'])
+      print_node = parse_print
+      return print_node
+    end
+
+    @position -= 1
+    variable_node = parse_variable_or_number
+    assign_operator = match(TokenTypesList::TOKEN_TYPES['ASSIGN'])
+
+    if assign_operator
+      right_formula_node = parse_formula
+      binary_node = BinOperationNode.new(assign_operator, variable_node, right_formula_node)
+      return binary_node
+    end
+
+    raise "After the variable, an assignment statement is expected at the position #{@position}"
+  end
+
+  def parse_print
+  end
+
+  def parse_variable_or_number
+  end
+
+  def parse_formula
+  end
+
+  def require_token(*expected)
     token = match(*expected)
     unless token
       raise "#{expected[0].name} expected at position #{@position}, but not found"
