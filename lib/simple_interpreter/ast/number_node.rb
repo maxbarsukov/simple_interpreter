eda43d8bbf8b987afa940948e1b4541a802b9744
maxbarsukov <maximbarsukov@bk.ru>
2021-05-26 20:49:43 +0300

    Variable and Number nodes created

--- lib/simple_interpreter/ast/number_node.rb
+++ lib/simple_interpreter/ast/number_node.rb
@@ -1,1 +1,9 @@
+require_relative 'expression_node'
 
+class NumberNode < ExpressionNode
+  attr_accessor :number
+
+  def initialize(number)
+    @number = number
+  end
+end
--- lib/simple_interpreter/ast/variable_node.rb
+++ lib/simple_interpreter/ast/variable_node.rb
@@ -1,1 +1,9 @@
+require_relative 'expression_node'
 
+class VariableNode < ExpressionNode
+  attr_accessor :variable
+
+  def initialize(variable)
+    @variable = variable
+  end
+end
