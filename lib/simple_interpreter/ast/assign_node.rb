78bb98a4130d753560d2653373200f4d1bd19a32
maxbarsukov <maximbarsukov@bk.ru>
2021-05-26 20:54:54 +0300

    Assign node created

--- lib/simple_interpreter/ast/assign_node.rb
+++ lib/simple_interpreter/ast/assign_node.rb
@@ -1,1 +1,13 @@
+require_relative 'expression_node'
 
+class AssignNode < ExpressionNode
+  attr_accessor :operator,
+                :left_node,
+                :right_node
+
+  def initialize(operator, left_node, right_node)
+    @operator = operator
+    @left_node = left_node
+    @right_node = right_node
+  end
+end
